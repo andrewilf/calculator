1ce35affa9f6a8f34cc80b5106bd5afeeed37d69
andrewfaulkner <andrewfau1kn3r@gmail.com>
2021-09-11 15:32:04 +0800

    cleaned code

--- app.js
+++ app.js
@@ -2,63 +2,34 @@ $(() => {
     let calcString = " "
     let decimalFlag = false
     let equalFlag = false
+
     const $number = $('.number').on('click', (event) => {
         let number = $(event.currentTarget).text()
         console.log(number)
         calcString += number
-        if(equalFlag)
-        {
+        if (equalFlag) {
             calcString = number
             equalFlag = false
         }
         updateVisual(calcString)
-        
+
     })
-    const $divide = $('#divide').on('click', (event) => {
-        console.log(calcString[calcString.length - 1])
+    const $action = $('.action').on('click', (event) => {
         if (!isNaN(parseInt(calcString[calcString.length - 1]))) {
             equalFlag = false
-            let number = $(event.currentTarget).text()
-            console.log(number)
-            calcString += " " + number + " "
-            updateVisual(calcString)
-            decimalFlag = false
-        }
-    })
-    const $multiply = $('#multiply').on('click', (event) => {
-        if (!isNaN(parseInt(calcString[calcString.length - 1]))) {
-            equalFlag = false
-            let number = $(event.currentTarget).text()
-            console.log(number)
-            calcString += " " + '*' + " "
-            updateVisual(calcString)
-            decimalFlag = false
-        }
-    })
-    const $minus = $('#minus').on('click', (event) => {
-        if (!isNaN(parseInt(calcString[calcString.length - 1]))) {
-            equalFlag = false
-            let number = $(event.currentTarget).text()
-            console.log(number)
-            calcString += " " + number + " "
-            updateVisual(calcString)
-            decimalFlag = false
-        }
-    })
-    const $plus = $('#plus').on('click', (event) => {
-        if (!isNaN(parseInt(calcString[calcString.length - 1]))) {
-            equalFlag = false
-            let number = $(event.currentTarget).text()
-            console.log(number)
-            calcString += " " + number + " "
+            let action = $(event.currentTarget).text()
+            if (action === 'x') {
+                action = '*'
+            }
+            console.log(action)
+            calcString += " " + action + " "
             updateVisual(calcString)
             decimalFlag = false
         }
     })
     const $decimal = $('#decimal').on('click', (event) => {
         console.log(parseInt(calcString[calcString.length - 1]))
-        if(equalFlag)
-        {
+        if (equalFlag) {
             calcString = "0."
             updateVisual(calcString)
             equalFlag = false
@@ -66,24 +37,19 @@ $(() => {
         else if (!isNaN(parseInt(calcString[calcString.length - 1])) && !decimalFlag) {
             equalFlag = false
             let number = $(event.currentTarget).text()
-            console.log(number)
             calcString += number
             updateVisual(calcString)
             decimalFlag = true
         }
 
     })
-    const $clear = $('#clear').on('click', (event) => {
+    const $clear = $('#clear').on('click', () => {
         equalFlag = false
-        let number = $(event.currentTarget).text()
-        console.log(number)
         calcString = ""
         updateVisual(calcString)
         decimalFlag = false
     })
-    const $equal = $('#equal').on('click', (event) => {
-        let number = $(event.currentTarget).text()
-        console.log(number)
+    const $equal = $('#equal').on('click', () => {
         let result = eval(calcString)
         calcString = result.toString()
         updateVisual(result)
